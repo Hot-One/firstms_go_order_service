c2d2229156c7b3331a30f704a63aea56aee87e8a
Abdulbosit <abdulbositkabilov1@gmail.com>
2023-08-10 16:36:27 +0500

    update storage

--- storage/storage.go
+++ storage/storage.go
@@ -1,9 +1,20 @@
 package storage
 
+import (
+	"context"
+
+	"github.com/Hot-One/firstms_go_order_service/genproto/user_service"
+)
+
 type StorageI interface {
 	CloseDB()
 	User() UserRepoI
 }
 
 type UserRepoI interface {
+	Create(context.Context, *user_service.UserCreate) (*user_service.User, error)
+	GetByID(context.Context, *user_service.UserPrimaryKey) (*user_service.User, error)
+	GetList(context.Context, *user_service.UserGetListRequest) (*user_service.UserGetListResponse, error)
+	Update(context.Context, *user_service.UserUpdate) (*user_service.User, error)
+	Delete(context.Context, *user_service.UserPrimaryKey) error
 }
